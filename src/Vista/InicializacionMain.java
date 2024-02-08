3ca760c4af229cd9542fa1e139c86396cfa16641
Abigail <abigailpm103@outlook.com>
2024-02-07 22:11:14 -0600

    Cambio de nombre interface

--- src/Vista/InicializacionMain.java
+++ src/Vista/InicializacionMain.java
@@ -14,13 +14,13 @@ import static java.awt.GridBagConstraints.BOTH;
  */
 
 
-public class InicioMain extends javax.swing.JFrame {
+public class InicializacionMain extends javax.swing.JFrame {
 
     /**
      * Creates new form InicioMain
      */
     Fuentes tipoFuente;
-    public InicioMain() {
+    public InicializacionMain() {
         initComponents();
         //this.setExtendedState(this.MAXIMIZED_BOTH);
         tipoFuente= new Fuentes();
@@ -92,20 +92,21 @@ public class InicioMain extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InicioMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicializacionMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InicioMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicializacionMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InicioMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicializacionMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InicioMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicializacionMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InicioMain().setVisible(true);
+                new InicializacionMain().setVisible(true);
             }
         });
     }
